4fd001b6db810d9342d9b595739f29fef5d26c37
Ahmed ZNOUDA <75784043+AhmedZNOUDA@users.noreply.github.com>
2021-06-10 15:10:52 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -52,6 +52,14 @@ app.use(function (err, req, res, next) {
     }
   });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000
 app.listen(port, () =>{
     console.log(`The server is running on port ${port}`)
